e63d3d7d3af5b068f0f53b1507b74859bd9e87b2
Marquis Kurt <software@marquiskurt.net>
2021-12-11 13:09:17 -0500

    Fix crash when filtered list of results is less than the specified count.

--- Shared/Sniglet.swift
+++ Shared/Sniglet.swift
@@ -13,80 +13,103 @@ class Sniglet {
     /// A class reference to the machine learning model.
     public typealias Validator = SnigletValidator
 
+    /// A class reference to the sniglet result type.
+    public typealias Result = SnigletResult
+
     /// A shared instance of the Sniglet class.
     public static var shared: Sniglet { Sniglet() }
 
+    // MARK: - PRIVATE FIELDS
     private var model: Validator
+    private var minChars: Int
+    private var maxChars: Int
+    private var batchSize: Int
+    private var syllablicStructs: SyllableShapes
 
-    /// A data structure that contains a sniglet validation result with an ID, validation check, and a confidence rate.
-    public struct Result: Hashable, Identifiable, Equatable {
-        /// The unique identifier for this result.
-        var id = UUID()
-
-        /// The word for which the result pertains to.
-        var word: String
-
-        /// The validation status of this result.
-        var validation: String
-
-        /// The confidence rate of the result.
-        var confidence: Double
-
-        /// Returns an empty result.
-        public static func empty() -> Result {
-            Result(word: "empty", validation: "valid", confidence: 0.0)
-        }
-
-        /// Returns a nullish result.
-        public static func null() -> Result {
-            Result(word: "null", validation: "valid", confidence: 0.0)
-        }
-
-        /// Returns an errored result.
-        public static func error() -> Result {
-            Result(word: "error", validation: "valid", confidence: 0.0)
-        }
-
-        public static func ==(lhs: Result, rhs: Result) -> Bool {
-            return lhs.word == rhs.word && lhs.validation == rhs.validation
-        }
-    }
+    // MARK: - CONSTRUCTOR
 
     init() {
         model = try! SnigletValidator(configuration: .init())
-    }
 
-    /// Returns a list of results from the sniglet validator.
-    /// - Parameter count: The number of words to create.
-    public func getNewWords(count: Int = 1) -> Set<Result> {
-        var generatedWords: [String] = []
-
-        var minof = UserDefaults.standard.integer(forKey: "algoMinBound")
-        if minof <= 0 {
-            minof = 3
+        // Grab the minimum number of letters from user preferences, or use the default value and set it.
+        minChars = UserDefaults.standard.integer(forKey: "algoMinBound")
+        if minChars <= 0 {
+            minChars = 3
             UserDefaults.standard.set(3, forKey: "algoMinBound")
         }
 
-        var maxof = UserDefaults.standard.integer(forKey: "algoMaxBound")
-        if maxof <= 0 {
-            maxof = 8
+        // Grab the maximum number of letters from user preferences, or use the default value and set it.
+        maxChars = UserDefaults.standard.integer(forKey: "algoMaxBound")
+        if maxChars <= 0 {
+            maxChars = 8
             UserDefaults.standard.set(8, forKey: "algoMaxBound")
         }
 
-        var batchSize = UserDefaults.standard.integer(forKey: "algoBatchSize")
+        // Grab the batch size from user preferences, or use the default value and set it.
+        batchSize = UserDefaults.standard.integer(forKey: "algoBatchSize")
         if batchSize <= 0 {
             batchSize = 25
             UserDefaults.standard.set(25, forKey: "algoBatchSize")
         }
 
-        var syllableStructs: SyllableShapes = .common()
+        // Load the syllable shapes in from the common and custom sets.
+        syllablicStructs = .common()
         if let customStructs: SyllableShapes = SyllableShapes(
             rawValue: UserDefaults.standard.string(forKey: "customShapes") ?? "") {
-            syllableStructs += customStructs
+            syllablicStructs += customStructs
+        }
+    }
+
+    // MARK: - GENERATION
+
+    /// Returns a list of results from the sniglet validator.
+    /// - Parameter count: The number of words to create.
+    public func getNewWords(count: Int = 1, from set: Set<Result> = Set()) -> Set<Result> {
+        // If our existing set already has the amount of words we need, return that here.
+        if set.count == count { return set }
+
+        // If the batch size is too close to the number of words we want to generate, use a different batch size to
+        // accomodate for it.
+        if (..<9).contains(batchSize - count) { batchSize *= 4 }
+
+        // Generate a batch of words.
+        let generatedWords = makeWordBatch(count)
+
+        // Create the empty word bank.
+        var wordResults: Set<Result> = set
+
+        // Try to validate all of the words against the model and return the word bank. If an error occurs, return a
+        // set with an Error tag.
+        do {
+            // Map them to inputs that the model will be able to understand.
+            let batchInput = generatedWords.map { word in word.asSnigletValidatorInput() }
+
+            // Predict each result, convert them into a result format the app can use, and only keep those that are
+            // valid.
+            var batchResults = try model.predictions(inputs: batchInput).enumerated()
+                .map { index, result in
+                    Result(word: generatedWords[index].replacingOccurrences(of: "*", with: ""),
+                           validation: result.Valid, confidence: result.ValidProbability["valid"] ?? 0)
+                }
+                .filter { result in result.validation == "valid" }
+
+            // Add words until our set is complete (or until we fill in all of our options).
+            while wordResults.count < count && batchResults.count > 0 {
+                wordResults.update(with: batchResults.removeFirst())
+            }
+        } catch {
+            return Set(arrayLiteral: .error())
         }
 
-        for _ in 0..<batchSize {
-            var newWord = String.makeWord(limit: minof..<maxof, with: syllableStructs.randomElement() ?? "CV")
+        // Return a recursive call of this function with the new set we just created to add more, if necessary.
+        return getNewWords(count: count, from: wordResults)
+    }
+
+    /// Generates a list of words with the specified count.
+    private func makeWordBatch(_ count: Int) -> [String] {
+        var generatedWords: [String] = []
+        for _ in 0..<count {
+            var newWord = String.makeWord(limit: minChars..<maxChars, with: syllablicStructs.randomElement() ?? "CV")
             if newWord.count < 8 {
                 for _ in newWord.count..<8 {
                     newWord += "*"
@@ -94,25 +117,6 @@ class Sniglet {
             }
             generatedWords.append(newWord)
         }
-
-        var wordResults: Set<Result> = Set()
-
-        do {
-            let batchInput = generatedWords.map { word in word.asSnigletValidatorInput() }
-            let batchResults = try model.predictions(inputs: batchInput).enumerated()
-                .map { index, result in
-                    Result(word: generatedWords[index].replacingOccurrences(of: "*", with: ""),
-                           validation: result.Valid, confidence: result.ValidProbability["valid"] ?? 0)
-                }
-                .filter { result in result.validation == "valid" }
-
-            while wordResults.count < count {
-                wordResults.update(with: batchResults.randomElement() ?? .null())
-            }
-            return wordResults
-        } catch {
-            return Set(arrayLiteral: .error())
-        }
-
+        return generatedWords
     }
 }
---
+++ Shared/SnigletResult.swift
@@ -0,0 +1,42 @@
+//
+//  SnigletResult.swift
+//  Give Me a Sniglet (iOS)
+//
+//  Created by Marquis Kurt on 11/12/21.
+//
+
+import Foundation
+
+/// A data structure that contains a sniglet validation result with an ID, validation check, and a confidence rate.
+struct SnigletResult: Hashable, Identifiable, Equatable {
+    /// The unique identifier for this result.
+    var id = UUID()
+
+    /// The word for which the result pertains to.
+    var word: String
+
+    /// The validation status of this result.
+    var validation: String
+
+    /// The confidence rate of the result.
+    var confidence: Double
+
+    /// Returns an empty result.
+    public static func empty() -> SnigletResult {
+        SnigletResult(word: "empty", validation: "valid", confidence: 0.0)
+    }
+
+    /// Returns a nullish result.
+    public static func null() -> SnigletResult {
+        SnigletResult(word: "null", validation: "valid", confidence: 0.0)
+    }
+
+    /// Returns an errored result.
+    public static func error() -> SnigletResult {
+        SnigletResult(word: "error", validation: "valid", confidence: 0.0)
+    }
+
+    public static func ==(lhs: SnigletResult, rhs: SnigletResult) -> Bool {
+        return lhs.word == rhs.word && lhs.validation == rhs.validation
+    }
+}
