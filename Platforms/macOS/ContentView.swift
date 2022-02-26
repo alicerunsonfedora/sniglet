7ba6326a6d7cbcb960bc739c79d861ed60cf28a5
Marquis Kurt <software@marquiskurt.net>
2022-02-26 15:53:49 -0500

    :construction: Add Mac target
    This new Mac target will be used to create the redesigned version
    of Give Me a Sniglet for macOS. The appropriate shared files
    have been added to the Mac target as well.
    
    ^ABY-11 stage Develop work Development 5m

---
+++ Platforms/macOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Give Me a Sniglet
+//
+//  Created by Marquis Kurt on 26/2/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
